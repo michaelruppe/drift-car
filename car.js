c1d4385e875c8cb95d5714fa2bcccf2c5da3ceb7
michaelruppe <michael.ruppe@live.com.au>
2019-04-13 13:09:48 +1000

    update drift mechanics, use WEBGL

--- car.js
+++ car.js
@@ -5,22 +5,22 @@ class Car {
     this.a = createVector(0,0);
     this.damping = createVector(0, 0);
     this.angle = 0;
-    this.turnRate = 0.1;
+    this.turnRateStatic = 0.1;
+    this.turnRateDynamic = 0.8 * this.turnRateStatic;
+    this.turnRate = this.turnRateStatic;
     this.f = 1;
     this.m = 5;
 
     this.w = 18;
     this.l = 30
 
-  }
+    this.DRIFT_CONSTANT = 1.5;
 
-  centreOnCar() {
-    push(); translate(this.d.x, this.d.y); rotate(this.angle);
   }
 
   show() {
     rectMode(CENTER);
-    this.centreOnCar();
+    push(); translate(this.d.x, this.d.y); rotate(this.angle);
     rect(0,0, this.w, this.l);
     rect(0, this.l/2, 2,2)
     pop();
@@ -53,21 +53,27 @@ class Car {
 
     // Car steering and drifting physics
     let bodyFixedVelocity = this.vectWorldToBody(this.v, this.angle);
-    // purely air resistance and axial drag
-    let bodyFixedDrag = createVector(bodyFixedVelocity.x * -2, bodyFixedVelocity.y * 0.05);
-    // let bodyFixedDrift = createVector(bodyFixedVelocity.x * 0.3, 0)
-    // bodyFixedDrag.add(bodyFixedDrift);
+
+    let bodyFixedDrag;
+    if ( abs(bodyFixedVelocity.x) < this.DRIFT_CONSTANT ) {
+      // Gripping
+      bodyFixedDrag = createVector(bodyFixedVelocity.x * -2, bodyFixedVelocity.y * 0.05);
+      this.turnRate = this.turnRateStatic;
+    } else {
+      // Drifting
+      bodyFixedDrag = createVector(bodyFixedVelocity.x * -0.5, bodyFixedVelocity.y * 0.05);
+      this.turnRate = this.turnRateDynamic;
+    }
+
+
+
+
     let worldFixedDrag = this.vectBodyToWorld(bodyFixedDrag, this.angle)
     this.a.add(worldFixedDrag.div(this.m)); // Include inertia
 
     // Physics Engine
-    // this.a.add(this.damping);
     this.angle = this.angle % TWO_PI; // Clamp angle to one revolution
     this.v.add(this.a);
-
-    // Fix numerical problems - tiny rounding errors accumulating. Can also think of this as static friction
-    // if (abs(this.v.x) < 1e-2) this.v.x = 0;
-    // if (abs(this.v.y) < 1e-2) this.v.y = 0;
 
     this.d.add(this.v);
     this.a = createVector(0,0);
--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
 let car;
 
 function setup() {
-  let canvas = createCanvas(600,600);
+  let canvas = createCanvas(600,600, WEBGL);
   canvas.parent('sketch-holder');
   car = new Car();
 
 }
 
 function draw() {
+  translate(-width/2, -height/2, 0);
+  frameRate(60);
   background(127);
 
   car.update();
